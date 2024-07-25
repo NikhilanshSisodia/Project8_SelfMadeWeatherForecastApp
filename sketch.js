cc996b6ce982f51ec071785c0ba51d6e73001309
NikhilanshSisodia <nikhilanshsisodia@gmail.com>
2024-07-25 13:28:23 +0530

    Updating Default Value

--- sketch.js
+++ sketch.js
@@ -51,3 +51,32 @@ async function getInfo() {
     }
 }
 
+onload = async () => {
+    let city = 'new delhi';
+    input.value = '';
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=767eeab86829dec1746548eaf932401d&units=metric`;
+    let res = await fetch(url)
+    let data = await res.json();
+
+    if (data.cod == 404) {
+        alert('Invalid City Name');
+    } else if (data.cod > 199 && data.cod < 300) {
+
+        temp.innerHTML = `${Math.round(data.main.temp)}°C`;
+        cityName.innerHTML = data.name;
+        windMeasure.innerHTML = `${Math.round(data.wind.speed)}km/hr`;
+        humidMeasure.innerHTML = `${Math.round(data.main.humidity)}%`;
+        weatherInfo.setAttribute(`src`, `./assets/${data.weather[0].toLowerCase().main}.png`);
+        weatherInfo.setAttribute(`alt`, `${data.weather[0].main}`);
+
+    } else if (data.cod > 399 && data.cod < 500) {
+
+        alert('Error from user side.');
+
+    } else if (data.cod > 499 && data.cod < 600) {
+
+        alert('Error from Server side.');
+
+    }
+}
